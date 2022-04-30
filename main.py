107c6223339b660b68c82b5342980f719e92f19e
Droniu <dron.official@yahoo.com>
2022-04-30 21:10:56 +0200

    Add /status endpoint

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+STATUS = True
 
 @app.get("/")
 async def root():
     return {"message": "Buslight recognition API"}
+
+@app.get("/status")
+async def get_status():
+    return {"status": STATUS}
