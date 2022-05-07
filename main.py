40fce065539c308dc5c79a7c7f3d2590e886a135
Droniu <dron.official@yahoo.com>
2022-05-07 19:12:56 +0200

    Wrap model with api

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import cv2
+import tensorflow as tf
+import numpy as np
 from fastapi import FastAPI
 
-app = FastAPI()
+from utils import convert, prepare
 
-STATUS = True
+app = FastAPI()
 
 @app.get("/")
 async def root():
@@ -10,4 +13,25 @@ async def root():
 
 @app.get("/status")
 async def get_status():
-    return {"status": STATUS}
+    return {"status": getPrediction()}
+
+model = tf.keras.models.load_model("mlp.model") 
+
+def getPrediction():
+    while True:
+        cap = cv2.VideoCapture(2) 
+
+        _, frame = cap.read()
+        print(frame);
+        # this is required for macbook webcams
+        if (not isinstance(frame, np.ndarray)):
+            continue
+        frame = convert(frame)
+
+        prediction = model.predict(prepare(frame)) 
+        print(prediction)
+                
+        cap.release()
+        break
+    result = True if prediction[0][0] < prediction[0][1] else False
+    return result
---
+++ utils.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+
+def point_trans_contrast_dynamic(image, m, e):
+    image = image.astype(np.float32) / 255
+    trans_image = 1/(1+(m/image)**e)
+    norm_image = cv2.normalize(trans_image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+    trans_image = norm_image.astype(np.uint8)
+    return trans_image
+
+def point_trans_gamma(image, const, gamma):
+    image = image.astype(np.float32) / 255
+    trans_image = (image**gamma)*const
+    norm_image = cv2.normalize(trans_image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+    trans_image = norm_image.astype(np.uint8)
+    return trans_image
+
+def crop(image ,x_a, x_b, y_a, y_b):
+    image_croped = image[x_a:x_b, y_a:y_b] 
+    return image_croped
+
+def convert(frame): #transformations used for lerning model
+    frame = cv2.resize(frame, (256, 180))                
+    frame = crop(frame, 0, 180, 38, 218) 
+    return frame
+    
+def prepare(frame):
+    return frame.reshape((-1, 97200))
